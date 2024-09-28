7c17c33044e56661acfc2d8594b5ee3b3b6fd3d5
hmndz3 <harrymndz3@gmail.com>
2024-09-28 17:02:20 -0600

    prueba

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
+    public  static void main(String[] args) {
         // Datos del Cliente
         boolean bandera1 = false;
         Cliente nuevo = null;
